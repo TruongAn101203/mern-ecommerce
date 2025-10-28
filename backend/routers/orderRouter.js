424bed4545df2dc660588d1fc988f10dfbc7bba8
Le Truong An <leantruong101203@gmail.com>
2025-10-28 20:37:57 +0700

    update: new orderrouter

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,17 +1,24 @@
 import express from "express";
-import { allOrders, placeOrder, placeOrderVnpay, userOrders, updateStatus } from "../controllers/orderController.js";
+import {
+  allOrders,
+  placeOrder,
+  placeOrderVnpay,
+  userOrders,
+  updateStatus
+} from "../controllers/orderController.js";
+
 import adminAuth from "../middleware/adminAuth.js";
 import authUser from "../middleware/auth.js";
 
+const orderRouter = express.Router();
 
-const orderRouter = express.Router()
-orderRouter.post('/list', adminAuth,allOrders )
-orderRouter.post('/status', adminAuth,updateStatus )
+// 🧭 Admin routes
+orderRouter.get("/list", adminAuth, allOrders);
+orderRouter.patch("/status", adminAuth, updateStatus);
 
-
-orderRouter.post("/place",authUser, placeOrder);
-orderRouter.post("/vnpay",authUser, placeOrderVnpay);
-
-orderRouter.post("/userorders",authUser, userOrders);
+// 🛒 User routes
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/vnpay", authUser, placeOrderVnpay);
+orderRouter.get("/userorders", authUser, userOrders);
 
 export default orderRouter;
